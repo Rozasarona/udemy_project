575d03e478f1d0d3cd20a8268711ec17ac03c2c7
Svetlana <example@gmail.com>
2023-10-24 11:46:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+const answer = +prompt("Вам есть 18?", "Впишите сюда свой возраст");
+console.log(typeof(answer));
+
